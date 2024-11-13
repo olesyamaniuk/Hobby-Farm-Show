ec78b7caa2796cd0a2abeee6c949ef48d0ccec33
olesyamaniuk <maniukolesya222@gmail.com>
2024-11-13 15:34:56 +0200

    1

---
+++ js/slick.js
@@ -0,0 +1,25 @@
+$(function () {
+  $('.play__list').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: 'linear',
+    nextArrow: '.play__button',
+    prevArrow: '.play__button',
+    
+  });
+});
+
+
+
+
+
+
+
